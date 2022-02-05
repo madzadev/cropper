5e2b3482235fb9a00bf8ab18b0ab8c5b699c7292
madzadev <madars.biss@gmail.com>
2022-02-05 12:04:46 +0200

    add drag modes

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>Cropper</h1>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default Header;
