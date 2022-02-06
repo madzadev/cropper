79af6907f6f76e9fbf95385dc16952f2d9613fa5
madzadev <madars.biss@gmail.com>
2022-02-06 17:59:59 +0200

    edit about page

--- pages/about.js
+++ pages/about.js
@@ -4,11 +4,32 @@ const About = () => {
   return (
     <ContentWrapper>
       <h1>This is About page</h1>
-      <p>Frontend - value your data privacy</p>
       <p>
-        The project was created as an entry for the Hashnode x Netlify
-        hackathon.
+        Have you ever created an awesome article with a great cover image.
+        Upload everything and BAAM, the{" "}
       </p>
+      <p>Sharing = you are not in full control of how the </p>
+
+      <h1>Privacy of the data</h1>
+      <p>
+        In this project the data privacy is a priority. All the cropping is done
+        in the frontend. No image is ever uploaded, its all you, your image and
+        your browser. Simple as that.
+      </p>
+
+      <h1>Additional notes</h1>
+      <p>
+        The project was created specifically as an entry for the Hashnode x
+        Netlify Hackathon.
+      </p>
+
+      <h1>Final notes</h1>
+      <p>
+        The technical formatting should be the last thing you should worry
+        about. Instead you should focus on what matters, creating quality
+        content.
+      </p>
+      <p>Enjoy using Cropper.</p>
     </ContentWrapper>
   );
 };
