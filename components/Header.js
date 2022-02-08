cb75afbfd763740d24d4425bdb5c9109b0070247
madzadev <madars.biss@gmail.com>
2022-02-08 22:49:32 +0200

    change import section

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,6 @@
-// import Image from "next/image";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import styles from "./Header.module.css";
-import { useRouter } from "next/router";
 
 const Header = () => {
   const router = useRouter();
