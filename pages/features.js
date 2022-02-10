63955f47daa2a2175f313710243773549a3b9e69
Madza <madars.biss@gmail.com>
2022-02-10 15:16:12 +0200

    features cleanup

--- pages/features.js
+++ pages/features.js
@@ -14,16 +14,6 @@ const Features = () => {
   )
 })}
       </div>
-      {/* <p>Image upload UI</p>
-      <p>Adjust the crop region</p>
-      <p>Move image in canvas (X and Y axis)</p>
-      <p>Rotate image (+/- 45 degree step)</p>
-      <p>Zoom image in or out (+/- 0.1 step)</p>
-      <p>Crop templates to fit popular platforms</p>
-      <p>Aspect ratio presets</p>
-      <p>Crop overview data</p>
-      <p>Image preview</p>
-      <p>Download as .png and .jpg formats</p> */}
     </ContentWrapper>
   );
 };
