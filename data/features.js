27615d44c6568b030ec210499581038c00a976d8
Madza <madars.biss@gmail.com>
2022-02-10 12:38:25 +0200

    edit feature data

--- data/features.js
+++ data/features.js
@@ -1,3 +1,62 @@
 export const features = [
-    
+    {
+        title:"Upload",
+        description: "Upload images in the popular formats such as .PNG and .JPG",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Presets",
+        description: "Currently there are 30 presets from 12 commonly used platforms",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Custom mode",
+        description: "User is allowed to switch to custom resolutions and aspect ratios",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Move tools",
+        description: "User can move the image around",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Zoom tools",
+        description: "Zoom the image in or out to get the precise crops",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Rotate image",
+        description: "Rotate image clockwise or counter-clockwise by 45 degree increments",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Transform image",
+        description: "Swap image on X or Y axis",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Crop data",
+        description: "Crop window size data. Updated in real time as you resize",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Crop score",
+        description: "Automatcally calculated crop score for the fit of the recommended resolutions",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Image Preview",
+        description: "The user is allowed to preview the crop.",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Reset",
+        description: "Reset image and crop windows as they were when the image was uploaded",
+        icon: "/graphics/features"
+    },
+    {
+        title:"Download",
+        description: "Download the image in popular formats such as .PNG and .JPG",
+        icon: "/graphics/features"
+    }
 ]
