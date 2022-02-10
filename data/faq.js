75a25fc182b7855c1f95b04c8f7b756560e16b3e
Madza <madars.biss@gmail.com>
2022-02-10 15:52:56 +0200

    swap data files

---
+++ data/faq.js
@@ -0,0 +1,8 @@
+// What is CropScore?
+// Why was it built?
+// How was it built?
+// Who built it?
+// Whats the license?
+// The code is available on ther GitHub repository.
+// Contribution are welcome.
+// The project is under MIT license.
