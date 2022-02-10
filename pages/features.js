92d9c2a2829bf00757baf7343a11a36ea1263861
madzadev <madars.biss@gmail.com>
2022-02-10 18:19:29 +0200

    add feature wrapper

--- pages/features.js
+++ pages/features.js
@@ -1,18 +1,22 @@
 import ContentWrapper from "../components/ContentWrapper";
+import FeatureCard from "../components/FeatureCard";
 import styles from "../styles/Features.module.css";
 
-import {features} from "../data/features.js"
+import { features } from "../data/features.js";
 
 const Features = () => {
   return (
     <ContentWrapper>
       <h1 className={styles.title}>The features of the CropScore</h1>
       <div className={styles.wrapper}>
-{features.map((feature, index)=>{
-  return (
-    <ContentWrapper title={feature.title} description={feature.description}/>
-  )
-})}
+        {features.map((feature, index) => {
+          return (
+            <FeatureCard
+              title={feature.title}
+              description={feature.description}
+            />
+          );
+        })}
       </div>
     </ContentWrapper>
   );
