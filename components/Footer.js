86ee16af1cace45be328b60b75d06158c7f3c727
madzadev <madars.biss@gmail.com>
2022-02-11 22:53:10 +0200

    edit footer text

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,10 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.wrapper}>
-      <p style={{ fontWeight: "bold" }}>Made with 💖 by Madza</p>
-      <p style={{ fontWeight: "bold" }}>Icons taken from Flaticon and React-icons</p>
-      <p style={{ fontWeight: "bold" }}>Powered by NextJS, React-cropper, ChakraUI, GitHub and Netlify</p>
+      <p className={styles.text}>Made with 💖 by Madza</p>
+      <p className={styles.text}>
+        Powered by NextJS, React-cropper and Netlify
+      </p>
     </div>
   );
 };
