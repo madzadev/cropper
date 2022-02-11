eddcc2d493298269f823274cc8e0e282767ed927
Madza <madars.biss@gmail.com>
2022-02-11 12:52:10 +0200

    edit footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <div className={styles.wrapper}>
       <p style={{ fontWeight: "bold" }}>Made with 💖 by Madza</p>
-      <p style={{ fontWeight: "bold" }}>Hosted on Netlify</p>
+      <p style={{ fontWeight: "bold" }}>Icons taken from Flaticon and React-icons</p>
+      <p style={{ fontWeight: "bold" }}>Powered by NextJS, React-cropper, ChakraUI, GitHub and Netlify</p>
     </div>
   );
 };
