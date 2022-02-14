36d2b1c70eca0ac9f1768f03e8d0114f547e9702
madzadev <madars.biss@gmail.com>
2022-02-14 17:31:43 +0200

    add footer links

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,25 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.wrapper}>
-      <p className={styles.text}>Made with 💖 by Madza</p>
       <p className={styles.text}>
-        Powered by NextJS, React-cropper and Netlify
+        Made with 💖 by{" "}
+        <a href="https://github.com/madzadev" target="_blank">
+          Madza
+        </a>
+      </p>
+      <p className={styles.text}>
+        Powered by{" "}
+        <a href="https://nextjs.org" target="_blank">
+          NextJS
+        </a>
+        ,{" "}
+        <a href="https://npmjs.com/package/react-cropper" target="_blank">
+          React-cropper
+        </a>{" "}
+        and{" "}
+        <a href="https://netlify.com" target="_blank">
+          Netlify
+        </a>
       </p>
     </div>
   );
