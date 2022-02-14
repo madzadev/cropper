a12cdf4c2e3e3376cf390b7d29b99e38ec1f91fc
madzadev <madars.biss@gmail.com>
2022-02-14 17:34:15 +0200

    add link styling

--- components/Footer.js
+++ components/Footer.js
@@ -5,21 +5,29 @@ const Footer = () => {
     <div className={styles.wrapper}>
       <p className={styles.text}>
         Made with 💖 by{" "}
-        <a href="https://github.com/madzadev" target="_blank">
+        <a
+          href="https://github.com/madzadev"
+          target="_blank"
+          className={styles.link}
+        >
           Madza
         </a>
       </p>
       <p className={styles.text}>
         Powered by{" "}
-        <a href="https://nextjs.org" target="_blank">
+        <a href="https://nextjs.org" target="_blank" className={styles.link}>
           NextJS
         </a>
         ,{" "}
-        <a href="https://npmjs.com/package/react-cropper" target="_blank">
+        <a
+          href="https://npmjs.com/package/react-cropper"
+          target="_blank"
+          className={styles.link}
+        >
           React-cropper
         </a>{" "}
         and{" "}
-        <a href="https://netlify.com" target="_blank">
+        <a href="https://netlify.com" target="_blank" className={styles.link}>
           Netlify
         </a>
       </p>
