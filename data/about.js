3d40507bc8caf7e8e94274007cab2408cce7587a
madzadev <madars.biss@gmail.com>
2022-02-19 09:13:14 +0200

    edit the credits

--- data/about.js
+++ data/about.js
@@ -8,23 +8,25 @@ export const about = [
   {
     question: "What is CropScore?",
     answers: [
-      "CropScore is an online image cropper specifically designed for content creators.",
-      "The importance is that you have media presets, so you do not have to worry about resolutions and aspect ratios",
+      "CropScore is an online image cropper designed for content creators.",
+      "It calculates the score of how well the real-time crop will fit for the target use.",
+      "Custom resolutions and aspect ratios are also supported.",
     ],
     image: "/graphics/about/scissors.png",
   },
   {
     question: "Why was it built?",
     answers: [
-      "The project is built specifically as an entry for the Hashnode and Netlify hackathon.",
-      "Besides that I always wanted to solve the issues with cropping images.",
+      "The aim was to prevent users to calculate everything manually and avoid creating multiple templates.",
+      "The project was built specifically as an entry for the Hashnode and Netlify hackathon.",
     ],
     image: "/graphics/about/idea.png",
   },
   {
     question: "How was it built?",
     answers: [
-      "The project is built with NextJS and react-cropper. Chakra UI is used for components.",
+      "The project is built with NextJS and react-cropper.",
+      "Chakra UI is used for components.",
       "The icons are taken from Flaticon and React icons library.",
       "Code is stored on GitHub and deployed on Netlify.",
     ],
@@ -33,7 +35,7 @@ export const about = [
   {
     question: "Can I contribute to the project?",
     answers: [
-      "Yes, the project is open source on GitHub. Bug reports and feature requests are welcome.",
+      "Yes, the project is open source on GitHub. The feature requests are welcome.",
       "The project is under MIT license.",
     ],
     image: "/graphics/about/code.png",
