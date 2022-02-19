7eddc2939ef9eac81dbdd49b8a7efa37d03d4c68
madzadev <madars.biss@gmail.com>
2022-02-19 14:58:43 +0200

    resp layout change

--- data/about.js
+++ data/about.js
@@ -17,7 +17,7 @@ export const about = [
   {
     question: "Why was it built?",
     answers: [
-      "I wanted to create an effective solution that would help content creators to format the images.",
+      "I wanted to create an effective solution that would provide automa.",
       "The project was built specifically as an entry for the Hashnode and Netlify hackathon.",
     ],
     image: "/graphics/about/idea.png",
