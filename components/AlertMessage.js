ad18a169757ad9d288726e4a5b2fabfb7341a5a0
madzadev <madars.biss@gmail.com>
2022-02-20 13:52:11 +0200

    edit components

--- components/AlertMessage.js
+++ components/AlertMessage.js
@@ -1,18 +1,10 @@
-import {
-  Alert,
-  AlertIcon,
-  //   AlertTitle,
-  AlertDescription,
-  CloseButton,
-} from "@chakra-ui/react";
+import { Alert, AlertIcon, AlertDescription } from "@chakra-ui/react";
 
 const AlertMessage = ({ message }) => {
   return (
     <Alert status="error">
       <AlertIcon />
-      {/* <AlertTitle mr={2}>Your browser is outdated!</AlertTitle> */}
       <AlertDescription>{message}</AlertDescription>
-      {/* <CloseButton position="absolute" right="8px" top="8px" /> */}
     </Alert>
   );
 };
--- components/CheckBox.js
+++ components/CheckBox.js
@@ -1,9 +1,6 @@
 import { Checkbox } from "@chakra-ui/react";
 
 const CheckBox = ({ index, title, isChecked, onChange }) => {
-  // return (
-  //   <input type="checkbox" onChange={onChange} defaultChecked={index == 1} />
-  // );
   return (
     <Checkbox
       isChecked={isChecked}
