8e73905c190fb0b2aad595c203e45b1cfd97ecc9
madzadev <madars.biss@gmail.com>
2022-02-21 08:22:46 +0200

    edit logos

--- components/ActionButton.js
+++ components/ActionButton.js
@@ -1,13 +1,19 @@
-import { Icon, Button } from "@chakra-ui/react";
+import { Icon, Button, useMediaQuery } from "@chakra-ui/react";
 
 const ActionButton = ({ onClick, icon, color, title }) => {
+  const [isMobile] = useMediaQuery("(min-width: 1300px)");
   return (
     <Button
       onClick={onClick}
-      leftIcon={<Icon as={icon} w={5} h={5} />}
+      leftIcon={isMobile && <Icon as={icon} w={5} h={5} />}
       colorScheme={color}
       variant="solid"
       justifyContent="flex-start"
+      // height={[`${text.length > 30 ? "60px" : "40px"}`, "40px"]}
+      style={{
+        whiteSpace: "normal",
+        wordWrap: "break-word",
+      }}
     >
       {title}
     </Button>
