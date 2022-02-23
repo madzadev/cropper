46e9dfdfea94fffed148f8d3037d16344d78b904
madzadev <madars.biss@gmail.com>
2022-02-23 20:55:48 +0200

    edit toggle bar

--- components/PresetsToggleBar.js
+++ components/PresetsToggleBar.js
@@ -13,7 +13,7 @@ const PresetsToggleBar = ({
     <Button
       onClick={onClick}
       rightIcon={<Icon as={!active ? openIcon : closeIcon} w={5} h={5} />}
-      colorScheme={color}
+      // colorScheme={color}
       className={styles.wrapper}
       variant="solid"
       justifyContent="flex-start"
@@ -22,6 +22,7 @@ const PresetsToggleBar = ({
         whiteSpace: "normal",
         wordWrap: "break-word",
         display: `${isMobile ? "block" : "none"}`,
+        backgroundColor: "#e5eafe",
       }}
     >
       {!active ? "Open Presets List" : "Close Presets List"}
